ed02c9c994d2ce75ad8d0dfb7545b2281430699f
leolulu <348699103@qq.com>
2021-10-19 10:54:22 +0800

    修复不能重复发送提醒的问题

--- restart_notify_44.py
+++ restart_notify_44.py
@@ -2,9 +2,10 @@ from messenger import ijingniu_sender
 from time import sleep
 
 
-for _ in range(20):
-    sleep(3)
+for i in range(20):
+    i = i + 1
     ijingniu_sender(
-        "44重启了！！！",
-        "可能由于系统更新等原因，反正机器重启了，快去手动启动录播姬！！！"
+        f"【{i}】44重启了！！！",
+        f"【{i}】可能由于系统更新等原因，反正机器重启了，快去手动启动录播姬！！！"
     )
+    sleep(3)
